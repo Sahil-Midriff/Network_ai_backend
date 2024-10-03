972aaa78e02483a3aa535c7059b7b0d91a0d6f55
KamalKaur16 <traineemidriff14@gmail.com>
2024-10-03 15:11:16 +0530

    a

--- network_project/network_app/models.py
+++ network_project/network_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class cuser(AbstractUser):
+    full_name         = models.CharField(max_length=100)
+    phone_no          = models.CharField(max_length=10,unique=True)
+    address           = models.TextField()
+    age               = models.IntegerField()
+
+    def __str__(self):
+        return self.full_name
