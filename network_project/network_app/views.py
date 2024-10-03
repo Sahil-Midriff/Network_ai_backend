9c0b146172145eb80c5c959944e734b7fad719f1
KamalKaur16 <traineemidriff14@gmail.com>
2024-10-03 14:42:42 +0530

    'TT'

--- network_project/network_app/views.py
+++ network_project/network_app/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse('Welcome to home')
+
+def signup(request):
+    return render(request,'./templates/signup.html')
+
