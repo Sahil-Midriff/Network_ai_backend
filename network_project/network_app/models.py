9dc4ff7cbb4e0f5abd4f6d35268335e8f090b89a
KamalKaur16 <traineemidriff14@gmail.com>
2024-10-03 15:58:30 +0530

    --

--- network_project/network_app/models.py
+++ network_project/network_app/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
-<<<<<<< HEAD
 
 class Cuser(AbstractUser):
     full_name         = models.CharField(max_length=100)
@@ -12,5 +11,3 @@ class Cuser(AbstractUser):
 
     def __str__(self):
         return self.full_name
-=======
->>>>>>> 664b7ec50a1fbc972fb42860c630e4526731a65d
