46005e342ddd724adb2a3cda5e61733ea2a65c53
sahil <midriffdeveloper22@gmail.com>
2024-10-03 17:49:00 +0530

    Added .gitignore and removed unnecessary files from tracking

--- network_project/network_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('home/',    home,name='home'),
-    path('signup/',  signup,name='signup'),
-    path('login/',   signup,name='signup'),
-]
