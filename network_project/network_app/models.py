a0a580fadeaef1f3646849acef821f76c2823b92
KamalKaur16 <traineemidriff14@gmail.com>
2024-10-03 17:48:08 +0530

    'KK'

--- network_project/network_app/models.py
+++ network_project/network_app/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 class Cuser(AbstractUser):
-    full_name         = models.CharField(max_length=100)
     phone_no          = models.CharField(max_length=10,unique=True)
     address           = models.TextField()
     age               = models.IntegerField()
